fe2602888a032bb224ebea78b5e523a7c7034179
rodmerigueti <rod.merigueti@gmail.com>
2023-10-30 19:50:15 -0300

    update l3q4; l3q6

---
+++ POO/python/Lista 03/l3q7.py
@@ -0,0 +1,11 @@
+"""
+Questão 7. Implemente um programa na linguagem Python contendo uma função que
+recebe como argumentos o caminho de dois arquivos. O primeiro arquivo contém nomes
+de alunos e o segundo arquivo contém as notas dos alunos. No primeiro arquivo, cada
+linha corresponde ao nome de um aluno e no segundo arquivo, cada linha corresponde às
+notas dos alunos (uma ou mais). Por exemplo, a primeira linha do primeiro arquivo tem
+o nome do aluno e suas notas estão na primeira linha do segundo arquivo e assim
+sucessivamente. As notas de cada aluno estão separadas umas das outras por espaços em
+branco. Leia os dois arquivos e gere um terceiro arquivo que contém o nome do aluno
+seguido da média de suas notas.
+"""
