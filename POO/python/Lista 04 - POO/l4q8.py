a42a5e0614cf965eb0717a79dafe4869ed12c6ec
rodmerigueti <rod.merigueti@gmail.com>
2023-12-01 19:31:12 -0300

    Questão 8

--- POO/python/Lista 04 - POO/l4q8.py
+++ POO/python/Lista 04 - POO/l4q8.py
@@ -32,40 +32,66 @@ class Grafo:
     def __init__(self):
         self.V = {}
 
-    def Inserir_vertice(self, _v , _A = []):
-        print('===========================')
-        print(f"2 {_A}")
-        A = _A
-        v = int(_v)
-        block = False
-        print(f"1 {v}")
-        if v not in self.V:
-            if A == []:
-                self.V[v] = A
+    def Inserir_vertice(self, _v):
+        if _v in self.V:
+            pass
+        else:
+            self.V[_v] = []
+
+    def Inserir_aresta(self, v1, v2):
+        if v1 in self.V and v2 in self.V:
+            if v1 not in self.V[v2]:
+                self.V[v1].append(v2)
+                self.V[v2].append(v1)
             else:
-                for i in A:
-                    if i not in self.V:
-                        block = True
-                if block == False:
-                    self.V[v] = A
+                pass #Já foi adicionado
+
+    def ListarVertices(self):
+        print("Vertice - Aresta")
+        for v in self.V:
+            print(f"{v} - {self.V[v]}")
+    
+    def ListarVizinhosDe(self, v):
+        for a in self.V[v]:
+            print(f"{a}")
+    
+    def ListarArestas(self): #não fiz tratamento para evitar repetição
+        for V in self.V:
+            for a in self.V[V]:
+                print(f"{a} ------ {V}")
+    
+    def sao_vizinhos(self, v1, v2):
+        if v1 in self.V[v2]:
             return True
         else:
             return False
-        
-    def ListarVertices(self):
-        return self.V
+    
     
 G = Grafo()
 
-G.Inserir_vertice(0)
-G.Inserir_vertice(1, [0])
-G.Inserir_vertice(2, [0, 1])
-G.Inserir_vertice(2, [0, 1]) #Não vai inserir pois nó V2 já foi adicionado
-G.Inserir_vertice(4, [3, 1]) #Não vai inserir pois não exist V3
+G.Inserir_vertice(1)
+G.Inserir_vertice(2)
+G.Inserir_vertice(3)
+G.Inserir_vertice(4)
+G.Inserir_vertice(5)
 
+G.Inserir_aresta(1, 2)
+G.Inserir_aresta(1, 5)
+G.Inserir_aresta(2, 1)#Essa aresta já foi adcionada
+G.Inserir_aresta(2, 5)
+G.Inserir_aresta(2, 4)
+G.Inserir_aresta(2, 3)
+G.Inserir_aresta(5, 4)
+G.Inserir_aresta(3, 4)
 
-print(G.ListarVertices())
-    
+G.ListarVertices()
+print(f"1 e 2 são vizinhos: {G.sao_vizinhos(1, 2)}")
+print(f"3 e 5 são vizinhos: {G.sao_vizinhos(5, 3)}")
+print(f"TODAS AS ARESTAS")
+G.ListarArestas()
+
+print("Quais são os vizinhos de 2")
+G.ListarVizinhosDe(2)
 
     
 
